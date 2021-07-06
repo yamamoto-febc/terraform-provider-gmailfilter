771654e8edc1306dbe74c56f976ebdf4e77583fc
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2021-07-06 11:58:26 +0900

    Added -debug flag to enable debugging of providers

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/yamamoto-febc/terraform-provider-gmailfilter/gmailfilter"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: gmailfilter.Provider,
-	})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/yamamoto-febc/gmailfilter",
+			&plugin.ServeOpts{
+				ProviderFunc: gmailfilter.Provider,
+			})
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: gmailfilter.Provider})
+	}
 }
